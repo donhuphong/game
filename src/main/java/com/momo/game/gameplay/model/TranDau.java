133618fa19f9e45ede522199ea1c49ebbb945d90
thongho98 <hoquocthong456@gmail.com>
2020-10-01 14:36:26 +0700

    entity

---
+++ src/main/java/com/momo/game/gameplay/model/TranDau.java
@@ -0,0 +1,49 @@
+package com.momo.game.gameplay.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table("traudau")
+public class TranDau {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private User user1;
+    private User user2;
+
+    @OneToMany
+    private Hiep hieps;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public void setUser1(User user1) {
+        this.user1 = user1;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
+
+    public void setUser2(User user2) {
+        this.user2 = user2;
+    }
+
+    public Hiep getHieps() {
+        return hieps;
+    }
+
+    public void setHieps(Hiep hieps) {
+        this.hieps = hieps;
+    }
+}
